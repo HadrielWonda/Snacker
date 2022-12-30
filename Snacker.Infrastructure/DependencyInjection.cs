2970dbb25f3f83b7d34b94fc7947e117fb844b25
Olawale Abaniwonda <olawalebash2017@gmail.com>
2022-12-30 02:29:54 +0100

    Add files via upload

---
+++ Snacker.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.Dependencyinjection.Abstractions;
+
+
+namespace Snacker.Infrastructure;
+
+public static class DependencyInjection
+{
+public static IServiceCollection.AddInfrastructure(this IServiceCollection services)
+{
+    
+    return services;
+}
+}
